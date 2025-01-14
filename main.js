702afb5861c492c60afb9601f45d11e08b875548
Maho Verhaeghe <maho.verhaeghe59@gmail.com>
2025-01-14 10:23:04 +0100

    Adding 4 import as API

--- main.js
+++ main.js
@@ -79,6 +79,7 @@ async function importDataFromApi(path) {
     try {
         const response = await fetch('http://localhost:8080/'+ path)
         const data = await response.json()
+        console.log(data)
         return data
     } catch (error) {
         console.error('Erreur:', error);
@@ -87,146 +88,111 @@ async function importDataFromApi(path) {
 }
 
 
-/**
- * METHODE TEMPORAIRE POUR POUVOIR IMPORTER DES DONNEES, RETOURNE DES DONNEES SOUS FORME D'OBJET
- * @param {*} path chemin vers l'api
- */
-async function importCSV(path) {
-    try {
-        const response = await fetch(path);
-        if (!response.ok) {
-            throw new Error(`Erreur de chargement : ${response.status}`);
-        }
-        const data = await response.text();
-
-        const lignes = data.trim().split('\n');
-        const enTete = lignes[0].split(',');
-
-        const objets = lignes.slice(1).map(ligne => {
-            const valeurs = ligne.split(',');
-            const objet = {};
-            enTete.forEach((cle, index) => {
-                objet[cle.trim()] = valeurs[index]?.trim();
-            });
-            return objet;
-        });
-
-        return objets;
-
-    } catch (error) {
-        console.error('Erreur lors de l\'import du CSV:', error);
-        return [];
-    }
-}
-// import DATA Implantation des arceaux vélos à Roubaix
 let parkings = [];
-importCSV("https://data.lillemetropole.fr/geoserver/ows?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=ville_roubaix%3Aimplantation_des_arceaux_velos_a_roubaix&OUTPUTFORMAT=csv")
-    .then((response) => {
-        response.forEach((place) => {
-            if (place['geom'].includes('POINT')) {
-                var coords = place['geom'].replace('(', '').replace(')', '').split(' ');
-                const lat = parseFloat(coords[1]);
-                const lon = parseFloat(coords[2]);
-                parkings.push({
-                    lat: lat,
-                    lon: lon
-                })
-                addMarker(lat, lon, place['localisation'], iconArceaux);
-            }
-        });
-    });
+
+// import DATA Implantation des arceaux vélos à Roubaix
+// importCSV("https://data.lillemetropole.fr/geoserver/ows?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=ville_roubaix%3Aimplantation_des_arceaux_velos_a_roubaix&OUTPUTFORMAT=csv")
+// .then((response) => {
+//         response.forEach((place) => {
+//             if (place['geom'].includes('POINT')) {
+//                 var coords = place['geom'].replace('(', '').replace(')', '').split(' ');
+//                 const lat = parseFloat(coords[1]);
+//                 const lon = parseFloat(coords[2]);
+//                 parkings.push({
+//                     lat: lat,
+//                     lon: lon
+//                 })
+//                 addMarker(lat, lon, place['localisation'], iconArceaux);
+//             }
+//         });
+//     });
 
 // import DATA Box à vélos à Lille, Lomme et Hellemmes
-importCSV("https://data.lillemetropole.fr/geoserver/ows?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=ville_lille%3Abox_a_velos_a_lille_lomme_et_hellemmes&OUTPUTFORMAT=csv")
-    .then((response) => {
+importDataFromApi('getBoxBike')
+    .then(response => {
         response.forEach((place) => {
-            if (place['FID'].includes('box')) {
-                var lat = parseFloat(place['x_l93']);
-                var lon = parseFloat(place['y_l93']);
-
-                var coords = convertLambert93ToWGS84(lat, lon);
-                parkings.push({
-                    lat: lat,
-                    lon: lon
-                })
-                addMarker(coords[0], coords[1], place['champ_calc'].split(': ')[1], iconBox);
-            }
+            var coords = [place['geometry']['coordinates'][0][1], place['geometry']['coordinates'][0][0]];
+            const lat = parseFloat(coords[0]);
+            const lon = parseFloat(coords[1]);
+            parkings.push({
+                lat: lat,
+                lon: lon
+            })
+            addMarker(lat, lon, place['properties']['localisati'], iconBox);
         });
-    });
+    })
+    .catch(error => console.error('Erreur lors de l\'import des données :', error));
 
 // import DATA Stations de réparations de vélo à Lille
-importCSV("https://data.lillemetropole.fr/geoserver/ows?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=ville_lille%3Abornes_dateliers_velos_a_lille&OUTPUTFORMAT=csv")
-    .then((response) => {
+importDataFromApi('getRepairBike')
+    .then(response => {
         response.forEach((place) => {
-            if (place['geom'].includes('POINT')) {
-                var coords = place['geom'].replace('(', '').replace(')', '').split(' ');
-                const lat = parseFloat(coords[1]);
-                const lon = parseFloat(coords[2]);
+            var coords = [place['geometry']['coordinates'][1], place['geometry']['coordinates'][0]];
+            const lat = parseFloat(coords[0]);
+            const lon = parseFloat(coords[1]);
 
-                addMarker(lat, lon, place['adresse'], iconStation);
-            }
+            addMarker(lat, lon, place['properties']['adresse'], iconStation);
         });
-    });
+    })
+    .catch(error => console.error('Erreur lors de l\'import des données :', error));
 
 //import DATA ilévia - Abris à vélos
-importCSV("https://data.lillemetropole.fr/geoserver/ows?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=dsp_ilevia%3Ailevia_abrisvelos&OUTPUTFORMAT=csv")
-    .then((response) => {
+importDataFromApi('getBikeShelters')
+    .then(response => {
         response.forEach((place) => {
-            if (place['geom'].includes('POINT')) {
-                var coords = place['geom'].replace('(', '').replace(')', '').split(' ');
-                const lat = parseFloat(coords[1]);
-                const lon = parseFloat(coords[2]);
-                parkings.push({
-                    lat: lat,
-                    lon: lon
-                });
-                addMarker(lat, lon, place['adresse'], iconAbriVelo);
-            }
+            var coords = [place['geometry']['coordinates'][1], place['geometry']['coordinates'][0]];
+            const lat = parseFloat(coords[0]);
+            const lon = parseFloat(coords[1]);
+            parkings.push({
+                lat: lat,
+                lon: lon
+            });
+            addMarker(lat, lon, place['properties']['adresse'], iconAbriVelo);
         });
-    });
+    })
+    .catch(error => console.error('Erreur lors de l\'import des données :', error));
 
 // import DATA Espaces de stationnement (TE et VAE en libre service)
-importCSV("https://data.lillemetropole.fr/geoserver/ows?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=mel_mobilite_et_transport%3Aedpm_vae_libreservice&OUTPUTFORMAT=csv")
-    .then((response) => {
-        response.forEach((place) => {
-            if (place['geom'].includes('POINT')) {
-                const lat = parseFloat(place['y_lat']);
-                const lon = parseFloat(place['x_long']);
-               /* parkings.push({
-                    lat: lat,
-                    lon: lon
-                })*/
-                // TODO Filtre et à désactiver par défaut (il y en a trop à afficher !!!)
-                // addMarker(lat, lon, place['nom_voie'], iconParkingLS);
-            }
-        });
-    });
+// importCSV("https://data.lillemetropole.fr/geoserver/ows?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=mel_mobilite_et_transport%3Aedpm_vae_libreservice&OUTPUTFORMAT=csv")
+//     .then((response) => {
+//         response.forEach((place) => {
+//             if (place['geom'].includes('POINT')) {
+//                 const lat = parseFloat(place['y_lat']);
+//                 const lon = parseFloat(place['x_long']);
+//                /* parkings.push({
+//                     lat: lat,
+//                     lon: lon
+//                 })*/
+//                 // TODO Filtre et à désactiver par défaut (il y en a trop à afficher !!!)
+//                 // addMarker(lat, lon, place['nom_voie'], iconParkingLS);
+//             }
+//         });
+//     });
 
 
 // import DATA V'Lille - Disponibilité en temps réel
-importCSV("https://data.lillemetropole.fr/geoserver/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=dsp_ilevia%3Avlille_temps_reel&OUTPUTFORMAT=csv")
-    .then((response) => {
+importDataFromApi('getVlille')
+    .then(response => {
         response.forEach((place) => {
-            if (place['geom'].includes('POINT')) {
-                var coords = place['geom'].replace('(', '').replace(')', '').split(' ');
-                const lat = parseFloat(coords[1]);
-                const lon = parseFloat(coords[2]);
-                boxes.push({
-                    lat: lat,
-                    lon: lon
-                });
-                var txt = place['adresse'];
-                if (place['etat'] == 'EN SERVICE') {
-                    txt += "<br>Vélos disponibles: " + place['nb_velos_dispo'] + "<br>Places disponibles: " + place['nb_places_dispo'];
+            var coords = [place['y'], place['x']];
+            const lat = parseFloat(coords[0]);
+            const lon = parseFloat(coords[1]);
+            // boxes.push({
+            //     lat: lat,
+            //     lon: lon
+            // });
+            var txt = place['adresse'];
+            if (place['etat'] == 'EN SERVICE') {
+                txt += "<br>Vélos disponibles: " + place['nb_velos_dispo'] + "<br>Places disponibles: " + place['nb_places_dispo'];
 
-                    addMarker(lat, lon, txt, iconVLilleOn);
-                } else {
-                    txt += "<br>Station hors service";
-                    addMarker(lat, lon, txt, iconVLilleOff);
-                }
+                addMarker(lat, lon, txt, iconVLilleOn);
+            } else {
+                txt += "<br>Station hors service";
+                addMarker(lat, lon, txt, iconVLilleOff);
             }
         });
-    });
+    })
+    .catch(error => console.error("Erreur lors de l'import des données :", error));
 
 
 
@@ -242,49 +208,51 @@ function addMarker(lat, lon, texte, icone) {
 // == Add Routing Functionality ==
 
 // Initialize the routing control
-let routingControl = L.Routing.control({
-    waypoints: [
-        L.latLng(50.62925, 3.057256),  // Starting point (can be dynamic)
-        L.latLng(50.62950, 3.05750)    // End point (can be dynamic)
-    ],
-    routeWhileDragging: true,   // Allow route to update while dragging
-    geocoder: L.Control.Geocoder.nominatim() // Geocoding for address lookup
-}).addTo(map);
+// let routingControl = L.Routing.control({
+//     waypoints: [
+//         L.latLng(50.62925, 3.057256),  // Starting point (can be dynamic)
+//         L.latLng(50.62950, 3.05750)    // End point (can be dynamic)
+//     ],
+//     routeWhileDragging: true,   // Allow route to update while dragging
+//     geocoder: L.Control.Geocoder.nominatim() // Geocoding for address lookup
+// }).addTo(map);
 
-function addRouteToNearestParking(userLat, userLon) {
-    let nearestParking = parkings[0];
-    let minDistance = Infinity;
+// function addRouteToNearestParking(userLat, userLon) {
+//     let nearestParking = parkings[0];
+//     let minDistance = Infinity;
 
-    parkings.forEach(parking => {
-        const distance = getDistance(userLat, userLon, parking.lat, parking.lon);
-        if (distance < minDistance) {
-            nearestParking = parking;
-            minDistance = distance;
-        }
-    });
+//     parkings.forEach(parking => {
+//         const distance = getDistance(userLat, userLon, parking.lat, parking.lon);
+//         if (distance < minDistance) {
+//             nearestParking = parking;
+//             minDistance = distance;
+//         }
+//     });
 
-    L.Routing.control({
-        waypoints: [
-            L.latLng(userLat, userLon),
-            L.latLng(nearestParking.lat, nearestParking.lon)
-        ]
-    }).addTo(map);
-}
+//     L.Routing.control({
+//         waypoints: [
+//             L.latLng(userLat, userLon),
+//             L.latLng(nearestParking.lat, nearestParking.lon)
+//         ]
+//     }).addTo(map);
+// }
 
-function getDistance(lat1, lon1, lat2, lon2) {
-    var R = 6371000; // Earth radius in meters
-    var φ1 = lat1 * Math.PI / 180;
-    var φ2 = lat2 * Math.PI / 180;
-    var Δφ = (lat2 - lat1) * Math.PI / 180;
-    var Δλ = (lon2 - lon1) * Math.PI / 180;
+// function getDistance(lat1, lon1, lat2, lon2) {
+//     var R = 6371000; // Earth radius in meters
+//     var φ1 = lat1 * Math.PI / 180;
+//     var φ2 = lat2 * Math.PI / 180;
+//     var Δφ = (lat2 - lat1) * Math.PI / 180;
+//     var Δλ = (lon2 - lon1) * Math.PI / 180;
 
-    var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
-        Math.cos(φ1) * Math.cos(φ2) *
-        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+//     var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+//         Math.cos(φ1) * Math.cos(φ2) *
+//         Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+//     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-    return R * c; // Return distance in meters
-}
+//     return R * c; // Return distance in meters
+// }
+
+
 // == Method calling ==
 
 if (navigator.geolocation) {
@@ -300,7 +268,7 @@ if (navigator.geolocation) {
             L.marker([lat, lon], { icon: iconUserPosition, interactive: false }).addTo(map);
 
             // Update the routing waypoints 
-            addRouteToNearestParking(lat, lon);
+            // addRouteToNearestParking(lat, lon);
 
         },
         function() {
--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -11,7 +11,7 @@ export async function getBikeShelters(req, res)
              },
           };
         const response = await axios.get('https://data.lillemetropole.fr/geoserver/ows?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=dsp_ilevia%3Ailevia_abrisvelos&OUTPUTFORMAT=application%2Fjson', config);
-        console.log(response.data.features);
+        console.log('Requête GET effectuée:', Vponse.data);
         res.status(200).json(response.data.features);
         } catch(error) {
             res.status(400).json({err: "Une erreur s'est produite. Reessayez"});
